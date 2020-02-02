bb24451c92974931d67d69330d6c335df474a55f
Sebastian Landwehr <info@dword-design.de>
2020-02-02 15:58:02 +0100

    upgrades

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { spawn } from 'child-process-promise'
-import nodeConfig from '@dword-design/base-config-node'
 
 export default {
-  dev: () => spawn('nuxt', ['--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
-  build: () => spawn('nuxt', ['build', '--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
-  start: () => spawn('nuxt', ['start', '--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
-  gitignore: [...nodeConfig.gitignore, '.linaria-cache', '.nuxt'],
+  gitignore: ['.linaria-cache', '.nuxt'],
+  commands: {
+    dev: () => spawn('nuxt', ['--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
+    prepublishOnly: () => spawn('nuxt', ['build', '--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
+    start: () => spawn('nuxt', ['start', '--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
+  },
 }
