5c744273dd94d8752470a3ef8dac4f4792a973a6
Sebastian Landwehr <info@dword-design.de>
2020-02-03 23:22:57 +0100

    add analyze command

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export default {
   commands: {
     dev: () => spawn('nuxt', ['--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
     prepublishOnly: () => spawn('nuxt', ['build', '--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
+    analyze: () => spawn('nuxt', ['build', '--analyze', '--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
     start: () => spawn('nuxt', ['start', '--config-file', require.resolve('./nuxt.config')], { stdio: 'inherit' }),
   },
 }
