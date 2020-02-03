9202de1e099f72a63e4a3d9e75bbd39b234cc79a
Sebastian Landwehr <info@dword-design.de>
2020-02-03 23:05:48 +0100

    add browser helper script for acss in dev

--- src/modules/acss.js
+++ src/modules/acss.js
@@ -1,4 +1,5 @@
 import serveStatic from 'serve-static'
+import getPackageName from 'get-package-name'
 
 export default function () {
   this.extendBuild(config => {
@@ -6,12 +7,15 @@ export default function () {
       .find(({ test }) => test.test('.js'))
       .use
       .unshift({
-        loader: require.resolve('webpack-atomizer-loader'),
+        loader: getPackageName(require.resolve('webpack-atomizer-loader')),
         query: {
           configPath: require.resolve('./config.js'),
         },
       })
   })
   this.options.head.link.push({ rel: 'stylesheet', href: '/acss.css' })
+  if (this.options.dev) {
+    this.options.head.script.push({ src: 'https://unpkg.com/acss-browser' })
+  }
   this.addServerMiddleware({ path: '/acss.css', handler: serveStatic('.acss/index.css') })
 }
