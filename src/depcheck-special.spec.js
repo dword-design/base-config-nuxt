a32348b345fdecfb5ae10ead72b26dc93ad46458
foo <foo@bar.de>
2020-05-12 16:22:14 +0200

    chore: refactor tests

--- src/depcheck-special.spec.js
+++ src/depcheck-special.spec.js
@@ -1,54 +1,54 @@
 import withLocalTmpDir from 'with-local-tmp-dir'
 import outputFiles from 'output-files'
-import { endent } from '@dword-design/functions'
+import { endent, mapValues } from '@dword-design/functions'
 import execa from 'execa'
 
+const runTest = ({ files, fail }) => () =>
+  withLocalTmpDir(async () => {
+    await outputFiles({
+      'depcheck.config.js': endent`
+      const special = require('../src/depcheck-special')
+      module.exports = {
+        specials: [
+          special,
+        ],
+      }
+    `,
+      ...files,
+    })
+    try {
+      await execa.command('depcheck --config depcheck.config.js')
+    } catch (error) {
+      expect(error.message).toMatch('Unused dependencies')
+      expect(fail).toBeTruthy()
+    }
+  })
+
 export default {
-  valid: () =>
-    withLocalTmpDir(async () => {
-      await outputFiles({
-        'depcheck.config.js': endent`
-        const special = require('../src/depcheck-special')
-        module.exports = {
-          specials: [
-            special,
-          ],
-        }
-      `,
-        'src/index.js': endent`
+  valid: {
+    files: {
+      'src/index.js': endent`
         export default {
           modules: [
             'foo',
           ],
         }
       `,
-        'package.json': JSON.stringify({
-          dependencies: {
-            foo: '^1.0.0',
-          },
-        }),
-      })
-      await execa.command('depcheck --config depcheck.config.js')
-    }),
-  'unused dependency': () =>
-    withLocalTmpDir(async () => {
-      await outputFiles({
-        'depcheck.config.js': endent`
-        const special = require('../src/depcheck-special')
-        module.exports = {
-          specials: [
-            special,
-          ],
-        }
-      `,
-        'package.json': JSON.stringify({
-          dependencies: {
-            foo: '^1.0.0',
-          },
-        }),
-      })
-      await expect(
-        execa.command('depcheck --config depcheck.config.js')
-      ).rejects.toThrow('Unused dependencies')
-    }),
-}
+      'package.json': JSON.stringify({
+        dependencies: {
+          foo: '^1.0.0',
+        },
+      }),
+    },
+  },
+  'unused dependency': {
+    files: {
+      'package.json': JSON.stringify({
+        dependencies: {
+          foo: '^1.0.0',
+        },
+      }),
+    },
+    fail: true,
+  },
+} |> mapValues(runTest)
