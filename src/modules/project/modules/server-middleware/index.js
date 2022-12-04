0ea2ac5b35a996755dfb66b90d21cfac24129edb
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-12-04 03:36:10 +0000

    fix: remove await

--- src/modules/project/modules/server-middleware/index.js
+++ src/modules/project/modules/server-middleware/index.js
@@ -1,20 +1,22 @@
+import babelConfig from '@dword-design/babel-config'
 import express from 'express'
 import mountFiles from 'express-mount-files'
 import P from 'path'
-import babelConfig from '@dword-design/babel-config'
 
-export default async function (options) {
+export default function (options) {
   const app = options.expressInstance || express()
-  app.use(mountFiles(P.join(this.options.srcDir, 'api'), {
-    paramChar: '_',
-    jitiOptions: {
-      esmResolve: true,
-      interopDefault: true,
-      transformOptions: {
-        babel: babelConfig,
+  app.use(
+    mountFiles(P.join(this.options.srcDir, 'api'), {
+      jitiOptions: {
+        esmResolve: true,
+        interopDefault: true,
+        transformOptions: {
+          babel: babelConfig,
+        },
       },
-    }
-  }))
+      paramChar: '_',
+    })
+  )
   this.addServerMiddleware({ handler: app, path: '/api' })
   this.options.watch.push(P.join(this.options.srcDir, 'api'))
 }
