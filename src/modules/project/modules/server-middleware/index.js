f4a90001a7ce70b147e231be1cd84f98d587a043
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-12-04 03:31:31 +0000

    fix: api jiti

--- src/modules/project/modules/server-middleware/index.js
+++ src/modules/project/modules/server-middleware/index.js
@@ -1,12 +1,20 @@
 import express from 'express'
 import mountFiles from 'express-mount-files'
 import P from 'path'
+import babelConfig from '@dword-design/babel-config'
 
 export default async function (options) {
   const app = options.expressInstance || express()
-  app.use(
-    await mountFiles(P.join(this.options.srcDir, 'api'), { paramChar: '_' })
-  )
+  app.use(mountFiles(P.join(this.options.srcDir, 'api'), {
+    paramChar: '_',
+    jitiOptions: {
+      esmResolve: true,
+      interopDefault: true,
+      transformOptions: {
+        babel: babelConfig,
+      },
+    }
+  }))
   this.addServerMiddleware({ handler: app, path: '/api' })
   this.options.watch.push(P.join(this.options.srcDir, 'api'))
 }
