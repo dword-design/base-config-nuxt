4f75af57140e48eb20a1472aa060fa6ef01ea37d
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-13 10:21:28 +0000

    fix: upgrades

--- src/dev.spec.js
+++ src/dev.spec.js
@@ -8,6 +8,7 @@ import outputFiles from 'output-files'
 import P from 'path'
 import portReady from 'port-ready'
 import kill from 'tree-kill-promise'
+import { puppeteerPrettyConsole } from 'puppeteer-pretty-console'
 
 import self from './dev.js'
 import config from './index.js'
@@ -30,6 +31,7 @@ export default tester(
       const nuxt = self()
       try {
         await portReady(3000)
+        puppeteerPrettyConsole(this.page)
         await this.page.goto('http://localhost:3000')
         let handle = await this.page.waitForSelector('.foo')
         expect(await handle.evaluate(el => el.textContent)).toEqual(
