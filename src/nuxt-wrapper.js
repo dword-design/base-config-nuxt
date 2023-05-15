55a935ddc46e92cce7032c26e5b7aaedcf6b2a82
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-15 18:11:32 +0000

    fix: remove warnings

--- src/nuxt-wrapper.js
+++ src/nuxt-wrapper.js
@@ -4,4 +4,4 @@ import dotenv from '@dword-design/dotenv-json-extended'
 import { execa } from 'execa'
 
 dotenv.config()
-execa('nuxt', process.argv.slice(2), { stdio: 'inherit' })
+execa('nuxt', process.argv.slice(2), { stdio: 'inherit', env: { NODE_ENV: '' } })
