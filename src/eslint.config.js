ba6166d39e1fbf4de8d783bede1f72c5966b0ee6
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-30 23:16:10 +0200

    fix: update eslint.config.js

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -2,6 +2,9 @@ import packageName from 'depcheck-package-name'
 
 export default {
   extends: packageName`@dword-design/eslint-config`,
+  "globals": {
+    "$fetch": "readonly",
+  },
   rules: {
     'import/no-unresolved': ['error', { ignore: ['#imports'] }],
     'import/no-webpack-loader-syntax': 'off',
