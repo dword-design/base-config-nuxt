32ac1b257158b1a969e4eff6603afeb58aaa8276
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-04-06 14:09:41 +0000

    fix: foo

--- src/manually-installed-modules/locale-link/index.js
+++ src/manually-installed-modules/locale-link/index.js
@@ -1,20 +1,11 @@
-import { addPlugin, addTemplate, createResolver } from '@nuxt/kit';
-import P from 'path';
+import { addComponent, createResolver, defineNuxtModule } from '@nuxt/kit';
 
 const resolver = createResolver(import.meta.url);
 
-export default () => {
-  addTemplate({
-    filename: P.join('nuxt-locale-link', 'component.vue'),
-    src: resolver.resolve('./component.vue'),
-    write: true,
-  });
-
-  addPlugin(
-    {
-      filename: P.join('nuxt-locale-link', 'plugin.js'),
-      src: resolver.resolve('./plugin'),
-    },
-    { append: true },
-  );
-};
+export default defineNuxtModule({
+  setup: () =>
+    addComponent({
+      filePath: resolver.resolve('./component.vue'),
+      name: 'NuxtLocaleLink',
+    }),
+});
