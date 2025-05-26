992d3af5dd5646c5b024cfbf5fd970602ae0c2b1
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-26 22:59:05 +0000

    fix: foo

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'eslint/config';
-
 import config from '@dword-design/eslint-config';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   config,
+  { rules: { 'import/no-unresolved': ['error', { ignore: ['#imports'] }] } },
   {
     files: ['eslint.config.js'],
     rules: { 'import/no-extraneous-dependencies': 'off' },
