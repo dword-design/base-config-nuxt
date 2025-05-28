369297495462475895981b6d21b927d5c88fee8d
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-28 19:23:09 +0200

    fix: foo

--- src/dev.js
+++ src/dev.js
@@ -1,13 +1,13 @@
 import { createRequire } from 'node:module';
 
-import { execa } from 'execa';
+import { x } from 'tinyexec';
 
 const _require = createRequire(import.meta.url);
 const nuxtWrapper = _require.resolve('./nuxt-wrapper.js');
 
 export default (options = {}) => {
   options = { log: process.env.NODE_ENV !== 'test', ...options };
-  return execa(nuxtWrapper, ['dev'], {
-    [options.log ? 'stdio' : 'stderr']: 'inherit',
+  return x(nuxtWrapper, ['dev'], {
+    nodeOptions: { [options.log ? 'stdio' : 'stderr']: 'inherit' },
   });
 };
