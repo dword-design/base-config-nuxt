df9119bb62e9a953b7dcdc94265590b3abf665ff
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-29 02:01:32 +0200

    chore: foo

--- src/dev.js
+++ src/dev.js
@@ -7,7 +7,7 @@ const nuxtWrapper = resolver.resolve('./nuxt-wrapper.js');
 
 export default (options = {}) => {
   options = { log: process.env.NODE_ENV !== 'test', ...options };
-  return execa(nuxtWrapper, ['dev'], {
+  return execa(nuxtWrapper, ['dev', '--no-fork'], {
     [options.log ? 'stdio' : 'stderr']: 'inherit',
     reject: false,
   });
