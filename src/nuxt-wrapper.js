72f9ba4147331e300a5cb596cec7d6aa4ec48d5e
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-30 02:01:08 +0000

    fix: foo

--- src/nuxt-wrapper.js
+++ src/nuxt-wrapper.js
@@ -6,6 +6,6 @@ import { execa } from 'execa';
 dotenv.config();
 
 execa('nuxt', process.argv.slice(2), {
-  reject: !['dev', 'start'].includes(process.argv[2]),
+  reject: process.env.NODE_ENV !== 'test' || !['dev', 'start'].includes(process.argv[2]),
   stdio: 'inherit',
 });
