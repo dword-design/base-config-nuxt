f15c664fcf237d4b979ede5112fb4ee8b1ebd1f5
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-30 15:53:56 +0000

    fix: foo

--- src/nuxt-wrapper.js
+++ src/nuxt-wrapper.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 execa('nuxt', process.argv.slice(2), {
   reject:
-    process.env.NODE_ENV !== 'test' || !['dev', 'start'].includes(process.argv[2]),
+    process.env.NODE_ENV !== 'test' ||
+    !['dev', 'start'].includes(process.argv[2]),
   stdio: 'inherit',
 });
--- src/start.js
+++ src/start.js
@@ -1,4 +1,9 @@
-import { execaCommand } from 'execa';
+import { createRequire } from 'node:module';
+
+import { execa } from 'execa';
+
+const resolver = createRequire(import.meta.url);
+const nuxtWrapper = resolver.resolve('./nuxt-wrapper.js');
 
 export default function (options) {
   options = {
