b5adf8ea3beabb1f3bb2894018b28ad92d1d9219
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-30 03:15:45 +0000

    fix: foo

--- src/lint.js
+++ src/lint.js
@@ -1,6 +1,6 @@
 import { createRequire } from 'node:module';
 
-import { execa, execaCommand } from 'execa';
+import { execa } from 'execa';
 
 const resolver = createRequire(import.meta.url);
 const nuxtWrapper = resolver.resolve('./nuxt-wrapper.js');
@@ -20,13 +20,13 @@ export default async function (options) {
     stderr: options.stderr,
   });
 
-  await execaCommand(
-    'stylelint --fix --allow-empty-input --ignore-path .gitignore **/*.{css,scss,vue}',
-    {
-      ...(options.log && { stdout: 'inherit' }),
-      cwd: this.cwd,
-      env: options.env,
-      stderr: options.stderr,
-    },
-  );
+  //await execaCommand(
+  //  'stylelint --fix --allow-empty-input --ignore-path .gitignore **/*.{css,scss,vue}',
+  //  {
+  //    ...(options.log && { stdout: 'inherit' }),
+  //    cwd: this.cwd,
+  //    env: options.env,
+  //    stderr: options.stderr,
+  //  },
+  //);
 }
