678ae5dde9bffd60dd9bab053d4660e90b0b5414
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-31 01:04:54 +0000

    chore: foo

--- src/start.spec.js
+++ src/start.spec.js
@@ -22,18 +22,13 @@ test('valid', async ({ page }, testInfo) => {
   const base = new Base(config, { cwd });
   await base.prepare();
   const port = await getPort();
-  console.log('got port');
   await base.run('prepublishOnly');
-  console.log('prepublishOnly done');
   const nuxt = base.run('start', { env: { PORT: port } });
 
   try {
-    console.log('waiting for port');
     await portReady(port);
-    console.log('port ready');
     await page.goto(`http://localhost:${port}`);
     await expect(page.locator('.foo')).toHaveText('Hello world');
-    console.log('test done');
   } finally {
     await killAndWait(nuxt, port);
   }
