7524f4a03314d017192656647a8cd41be04af70d
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-05-31 01:07:04 +0000

    chore: foo

---
+++ src/build.spec.js
@@ -0,0 +1,34 @@
+import P from 'node:path';
+
+import { Base } from '@dword-design/base';
+import { endent } from '@dword-design/functions';
+import { expect, test } from '@playwright/test';
+import { execaCommand } from 'execa';
+import fs from 'fs-extra';
+import outputFiles from 'output-files';
+
+import config from './index.js';
+
+test('cli', async ({}, testInfo) => {
+  const cwd = testInfo.outputPath('');
+
+  await outputFiles(cwd, {
+    model: {
+      'cli.js': endent`
+        #!/usr/bin/env node
+
+        import foo from './foo.js'
+
+        console.log(foo)
+      `,
+      'foo.js': "export default 'foo'",
+    },
+  });
+
+  const base = new Base(config, { cwd });
+  await base.prepare();
+  await base.run('build');
+  await fs.chmod(P.join(cwd, 'dist', 'cli.js'), '755');
+  const { stdout } = await execaCommand('./dist/cli.js', { cwd });
+  expect(stdout).toMatch(/^foo$/m);
+});
---
+++ src/lint.spec.js
@@ -0,0 +1,23 @@
+import pathLib from 'node:path';
+
+import { Base } from '@dword-design/base';
+import { expect, test } from '@playwright/test';
+import fs from 'fs-extra';
+
+import config from './index.js';
+
+test('css error', async ({}, testInfo) => {
+  const cwd = testInfo.outputPath('');
+  await fs.outputFile(pathLib.join(cwd, 'assets', 'style.scss'), 'foo bar\n');
+  const base = new Base(config, { cwd });
+  await base.prepare();
+  await expect(base.lint({ stderr: 'pipe' })).rejects.toThrow('CssSyntaxError');
+});
+
+test('ignored', async ({}, testInfo) => {
+  const cwd = testInfo.outputPath('');
+  const base = new Base(config, { cwd });
+  await base.prepare();
+  await fs.outputFile(pathLib.join(cwd, 'coverage', 'foo.scss'), 'foo bar\n');
+  await base.lint();
+});
