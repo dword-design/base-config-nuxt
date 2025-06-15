2b46ee694855f0f557b966540ce370398b525ece
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-06-15 22:59:38 +0200

    fix: foo

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,29 @@
 import { createJiti } from 'jiti';
 
-const jiti = createJiti(process.cwd(), { interopDefault: true });
+/**
+ * For some reason when using a dynamic import, the config.ts file will sometimes stay
+ * or be regenerated into the Playwright test output directory.
+ * 
+import pathLib from 'node:path';
+import { pathToFileURL } from 'node:url';
+
+const modulePath = pathLib.join(process.cwd(), 'config.ts');
+const moduleUrl = pathToFileURL(modulePath).href;
+
+const { default: config } = await import(moduleUrl).catch(error => {
+  if (error.message.startsWith(`Cannot find module '${modulePath}'`)) {
+    return { default: {} };
+  } else {
+    throw error;
+  }
+});
+ */
+
+const jiti = createJiti(process.cwd());
 let config = {};
 
 try {
-  config = jiti('./config.ts');
+  config = await jiti.import('./config.ts', { default: true });
 } catch (error) {
   if (!error.message.startsWith(`Cannot find module './config.ts'`)) {
     throw error;
