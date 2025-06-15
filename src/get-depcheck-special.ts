06bc7a3e21f413347d3412e395e58b83f4ada1a7
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-06-16 00:29:33 +0200

    fix: use new jiti import

--- src/get-depcheck-special.ts
+++ src/get-depcheck-special.ts
@@ -4,14 +4,12 @@ import { createJiti } from 'jiti';
 import requirePackageName from 'require-package-name';
 
 export default ({ cwd = '.' }) =>
-  path => {
+  async path => {
     if (pathLib.basename(path) === 'config.ts') {
       // TODO: Check full path including cwd so that we check config.ts at project root
-      const jitiInstance = createJiti(pathLib.resolve(cwd), {
-        interopDefault: true,
-      });
+      const jiti = createJiti(pathLib.resolve(cwd));
 
-      const config = jitiInstance('./config.ts');
+      const config = await jiti.import('./config.ts', { default: true });
 
       const modules = [
         ...(config.modules || []),
