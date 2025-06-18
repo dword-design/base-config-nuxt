0b398ffa3d04406f5ab5e421971968c91173c59e
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-06-18 22:24:30 +0200

    fix: foo

--- src/index.ts
+++ src/index.ts
@@ -110,6 +110,8 @@ export default function (config) {
     },
     useJobMatrix: true,
   };
+  "hasTypescriptConfigRootAlias": false,
+  "typescriptConfig": { "extends": "./.nuxt/tsconfig.json" },
 }
 
 export { default as getEslintConfig } from './get-eslint-config';
