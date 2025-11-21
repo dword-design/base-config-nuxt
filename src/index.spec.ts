084c001ce68545d7b6520da202b6fb241cad1fc8
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-11-21 11:37:54 +0100

    chore: foo

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -53,12 +53,13 @@ test('basic auth', async ({}, testInfo) => {
             .toString("utf8")
             .split(":");
 
-          const users = Array.isArray(config.users)
+          /*const users = Array.isArray(config.users)
             ? config.users
             : config.users.split(config.usersDelimiter ?? ",").map((user) => {
                 const [username, password] = user.split(":");
                 return { username, password };
-              });
+              });*/
+          console.log(config.users)
 
           /*authenticated = users.some(
             (user) => user.username === username && user.password === password,
